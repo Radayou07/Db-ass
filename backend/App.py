61e689df6ee5c0a9ad8eaeb5cc66a91e0590ef90
RA Dayou <85465760+Radayou07@users.noreply.github.com>
2026-06-04 15:33:39 +0700

    Update App.py to use PORT environment variable and disable debug mode for production

--- backend/App.py
+++ backend/App.py
@@ -52,4 +52,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    port = int(os.getenv("PORT", 5001))
+    app.run(debug=False, host="0.0.0.0", port=port)
